72b42a4999711e18b5302adac2f4ecc35f98592e
Vikram Kumar <vikcanada90@gmail.com>
2025-06-09 15:37:43 -0600

    css fixes

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -32,7 +32,7 @@ export default function AuthPage() {
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+    <div className="flex min-h-screen flex-col items-center justify-center">
       {currentStep === "email" && (
         <EmailEntryCard onUserExists={handleUserExists} />
       )}
